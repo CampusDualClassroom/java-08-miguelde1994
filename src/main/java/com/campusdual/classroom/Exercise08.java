80f6149aac3b40a5947f872e8faeeab19699be2e
Miguel Dagraza <migueldagraza@gmail.com>
2024-11-20 14:27:01 +0100

    ej8

--- src/main/java/com/campusdual/classroom/Exercise08.java
+++ src/main/java/com/campusdual/classroom/Exercise08.java
@@ -3,7 +3,8 @@ package com.campusdual.classroom;
 public class Exercise08 {
 
     public static void main(String[] args) {
-
+        //Desde el método main() se llamará al método whileLoop() pasándole un valor "Y" por parámetro.
+        whileLoop(5);
     }
 
     //TODO ↓
@@ -12,6 +13,15 @@ public class Exercise08 {
     // X+1 < Y. El próximo ciclo valdrá: X+2
     // etc.
     public static void whileLoop(int num) {
+        //El método whileLoop() recibe un número entero por parámetro ("Y"). Dentro del método,
+        // utilizando un bucle while, se llevarán a cabo comprobaciones cíclicas partiendo de cero ("X").
+        // Mientras que "X" sea menor que "Y" se mostrará un mensaje que además incluya información sobre cuánto valdrá X
+        // en la siguiente iteración, y se aumentará "X" en una unidad.
+       int i=0;
+        while (i<num) {
+            System.out.println(i + " < " + num + ". El proximo ciclo valdra: " + (i + 1));
+            i++;
+        }
 
     }
 }
